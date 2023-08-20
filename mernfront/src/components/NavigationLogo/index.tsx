1e467051f9506000fcbfbde50f6508421e217104
parazitas36 <lukaslg96@gmail.com.com>
2023-08-20 11:48:28 +0300

    Navbar logo can redirect to the homepage.

--- mernfront/src/components/NavigationLogo/index.tsx
+++ mernfront/src/components/NavigationLogo/index.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
 
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Box, Typography } from '@mui/material';
+import { Box, IconButton, Typography } from '@mui/material';
+import { useNavigate } from 'react-router';
 
 const NavigationLogo: React.FC = () => {
+	const navigate = useNavigate();
+
 	return (
 		<Box
 			flexGrow={1}
-			sx={{ display: 'flex' , alignItems: 'center'}}
+			sx={{ display: 'flex', alignItems: 'center' }}
 		>
-			<Typography
-				variant="h4"
-				noWrap
-				sx={{
-					fontFamily: 'lato',
-					letterSpacing: '.3rem',
-					color: 'inherit',
-					textDecoration: 'none',
-				}}
-			>
-				MERN E-SHOP
-			</Typography>
-			<ShoppingCartIcon sx={{ fontSize: 30, ml: '.3em' }} />
+			<IconButton sx={{ borderRadius: 5 }} onClick={() => navigate('/')}>
+				<Typography
+					variant="h4"
+					noWrap
+					sx={{
+						fontFamily: 'lato',
+						letterSpacing: '.3rem',
+						color: 'white',
+						textDecoration: 'none',
+					}}
+				>
+					MERN E-SHOP
+				</Typography>
+				<ShoppingCartIcon sx={{ fontSize: 30, ml: '.3em', color: 'white', }} />
+			</IconButton>
 		</Box>
 	);
 };
